17bb11a1b2cface2383764aea116c92a6d0c68cf
EBaugh <elvis.baugh@fourth.com>
2019-06-21 13:04:47 +0100

    chenhws

--- GiftAidCalculator.TestConsole/Properties/AssemblyInfo.cs
+++ GiftAidCalculator.TestConsole/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3f78da48-61ed-486b-9322-0b654408027b")]
